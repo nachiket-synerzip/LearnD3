8d46b265eee8a776d962f2d5aac72a12acb82f74
Nachiket <nachiket@ultp-133.(none)>
2013-05-17 10:25:56 +0530

    Projection according to the Albers equal-area conic projection

--- js/d3-samples.js
+++ js/d3-samples.js
@@ -7,7 +7,17 @@
 
 d3.json("data/uk.json", function(error, uk) {
   console.log(uk);
-  svg.append("path")
-      .datum(topojson.feature(uk, uk.objects.subunits))
-      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+  console.log(d3.geo);
+  var subunits = topojson.feature(uk, uk.objects.subunits),
+  //projection = d3.geo.mercator();//.scale(500).translate([width / 2, height]),
+  // Albers equal-area conic projection : http://en.wikipedia.org/wiki/Albers_projection
+  projection = d3.geo.albers()
+    .center([0, 55.4])
+    .rotate([4.4, 0])
+    .parallels([50, 60])
+    .scale(6000)
+    .translate([width / 2, height / 2]);
+  
+  path = d3.geo.path().projection(projection);
+  svg.append("path").datum(subunits).attr("d", path);
 });
